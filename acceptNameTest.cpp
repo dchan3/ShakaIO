a24caa2c504ae01b88150ba32a7ccb91dc7c2ec0
paulolemus <paulolemus14@gmail.com>
2017-02-07 17:26:59 -1000

    Added acceptname overload

--- acceptNameTest.cpp
+++ acceptNameTest.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
 
     char c = 'a';
-    bool accept = acceptName(c);
-    if(accept) cout << "ACEPT" << endl;
+    bool accept = acceptName();
+    if(accept) cout << "ACCEPT" << endl;
     else cout << "REJECT" << endl;
 
     return 0;
