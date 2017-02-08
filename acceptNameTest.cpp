fc9e81d690671540bfe63e14f88ae5ff7328ef9f
dchan3 <dchan3@hawaii.edu>
2017-02-07 17:26:22 -1000

    Changed spelling error in main function

--- acceptNameTest.cpp
+++ acceptNameTest.cpp
@@ -5,7 +5,7 @@ int main(){
 
     char c = 'a';
     bool accept = acceptName(c);
-    if(accept) cout << "ACEPT" << endl;
+    if(accept) cout << "ACCEPT" << endl;
     else cout << "REJECT" << endl;
 
     return 0;
